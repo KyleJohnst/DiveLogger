325830e8ca4ac1be6c5c511c3415e3c1d723d04d
Kyle Johnston <kyle9657@gmail.com>
2019-05-02 19:30:27 +0100

    Fixed data loader so that it actually loads data

--- src/main/java/com/example/kyle/DiveLogger/components/DataLoader.java
+++ src/main/java/com/example/kyle/DiveLogger/components/DataLoader.java
@@ -7,7 +7,9 @@ import com.example.kyle.DiveLogger.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
@@ -24,5 +26,5 @@ public class DataLoader implements ApplicationRunner {
         locationRepository.save(location);
         Dive dive = new Dive("BoatShed", location, 34, 15, "42");
         diveRepository.save(dive);
-    }
+}
 }
