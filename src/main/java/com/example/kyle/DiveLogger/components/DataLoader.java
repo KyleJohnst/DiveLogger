72a891b5556a8c4c8487ec6a81e80b454181cd3a
Kyle Johnston <kyle9657@gmail.com>
2019-05-02 19:15:47 +0100

    Adds dataloader

---
+++ src/main/java/com/example/kyle/DiveLogger/components/DataLoader.java
@@ -0,0 +1,28 @@
+package com.example.kyle.DiveLogger.components;
+
+import com.example.kyle.DiveLogger.models.Dive;
+import com.example.kyle.DiveLogger.models.Location;
+import com.example.kyle.DiveLogger.repositories.DiveRepository;
+import com.example.kyle.DiveLogger.repositories.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    @Autowired
+    DiveRepository diveRepository;
+
+    public DataLoader() {
+    }
+
+    public void run(ApplicationArguments args){
+        Location location = new Location("Scotland");
+        locationRepository.save(location);
+        Dive dive = new Dive("BoatShed", location, 34, 15, "42");
+        diveRepository.save(dive);
+    }
+}
