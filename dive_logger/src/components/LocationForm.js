d685bed8b423ccd02d41fa8c64eff723ad512fe6
Kyle Johnston <kyle9657@gmail.com>
2019-05-05 12:14:49 +0100

    Addded location detail with dive details and SAC

---
+++ dive_logger/src/components/LocationForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LocationForm = () => {
+
+    return(
+        <h3>Add New Location Form</h3>
+    )
+}
+export default LocationForm;
--- dive_logger/src/containers/LocationContainer.js
+++ dive_logger/src/containers/LocationContainer.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import LocationList from '../components/LocationList';
+import LocationForm from '../components/LocationForm';
 
 class LocationContainer extends Component {
     render(){
         return(
             <>
             <h3>LocationContainer</h3>
-            < LocationList viewLocation = {this.props.viewLocation} 
+            <LocationForm/>
+            <LocationList viewLocation = {this.props.viewLocation} 
             locations = {this.props.locations}
             /> 
             </>
